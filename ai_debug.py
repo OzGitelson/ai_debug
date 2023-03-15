3564fc3501281b46b090965462e53b58e86dfe82
Oz Gitelson <ogitelson@gmail.com>
2023-03-15 14:50:18 -0400

    add multilanguage support

--- ai_debug.py
+++ ai_debug.py
@@ -3,26 +3,31 @@ import os
 import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-first_line="#### Explain why the error message is occuring in the code given, what line it is occuring at, and give a potential modification to the existing code that could fix it"
+first_line="Explain why the error message is occurring in the code given, what line it is occurring at, " \
+           "and give a potential simple modification to the existing code that could fix it. Only explain the error in the " \
+           "code that led to the Error, and ignore any other errors you might see in the code"
 
-def generate_code_prompt(error, code):
-    return """{f}
-
+def generate_code_prompt(error, code, lang):
+    return """#### {f}
+    
+    ### Language
+    {l}
+    
     ### Error
     {e}
 
     ### Code
     {c}
 
-    ### Explanation of error""".format(f=first_line, e=error, c=code)
+    ### Explanation of error""".format(f=first_line, l=lang, e=error, c=code)
 
-def write_code(error, code):
+def write_code(error, code, lang):
     response = openai.Completion.create(
         model="code-davinci-002",
-        prompt=generate_code_prompt(error, code),
+        prompt=generate_code_prompt(error, code, lang),
         temperature=0.0,
         max_tokens=350,
-        stop=first_line
+        stop=[first_line, '```\n']
     )
     # print(response)
     with open('test.txt', 'w') as out:
@@ -30,9 +35,15 @@ def write_code(error, code):
         print("\n AI insight:\n", response['choices'][0]['text'])
 
 
-def debug(error, code):
-    with open('stderr_reader.py', 'r') as infile:
-        code = '\n'.join(infile.readlines())
+def debug(error, code, lang):
+    # error="""
+    # test.c:1:7: error: expected declaration specifiers or ‘...’ before string constant
+    # 1 | print("hello world");
+    #   |       ^~~~~~~~~~~~~
+    # make: *** [makefile:26: Test] Error 1
+    # """
+    #
+    # code = 'print("hello world");'
         # openai.api_key = KEY
         # openai.api_key = os.environ.get("OPENAI_API_KEY")
-        write_code(error, code)
+    write_code(error, code, lang)
--- stderr_reader.py
+++ stderr_reader.py
@@ -5,18 +5,32 @@ import ai_debug
 
 FOV=10
 
-if len(sys.argv)<2:
+if len(sys.argv)<3:
     print("Invalid command args")
     exit(1)
 
-path_to_python=sys.argv[1]
+command=sys.argv[1]
+lang=sys.argv[2]
 
-sub = Popen([r"C:\python projects\openai_api_test\openai-quickstart-python\venv\Scripts\python.exe", path_to_python], stdout=PIPE, stderr=PIPE)
+lang_dic={
+    'python':".py",
+    'C':".c",
+    'C++':'.cpp'
+
+}
+
+if lang not in lang_dic:
+    print("Invalid language")
+    exit(1)
+
+# sub = Popen([r"C:\python projects\openai_api_test\ai_debug\venv\Scripts\python.exe", path_to_python], stdout=PIPE, stderr=PIPE)
+sub = Popen(command.split(" "), stdout=PIPE, stderr=PIPE)
+
 output, error_output = sub.communicate()
 
 error_output=error_output.decode('utf_8')
 
-line_reg=re.compile(r'File "([^\n]+)", line ([0-9]+)')
+line_reg=re.compile('([^\n"]+{ext})[^\n0-9]+([0-9]+)'.format(ext=lang_dic[lang]))
 
 first_error=line_reg.search(error_output)
 
@@ -31,7 +45,7 @@ if first_error is not None:
                 code_to_send+=l
 
         # print(code_to_send)
-        ai_debug.debug(error_output, code_to_send)
+        ai_debug.debug(error_output, code_to_send, lang)
 else:
     print("No errors detected!")
 
